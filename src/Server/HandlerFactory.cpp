10076c8964a5b9bc1f6928c074da410d9d75f35a
liangxinglong <liangxinglong@scutech.com>
2023-08-09 15:37:41 +0800

    修改工厂类创建handler

--- src/Server/HandlerFactory.cpp
+++ src/Server/HandlerFactory.cpp
@@ -1,83 +1,79 @@
 #include "HandlerFactory.h"
 #include "LoginManager.h"
-#include "FileTransmission.h"
 #include "FileManageRequest.h"
+#include "FileTransmission.h"
+#include "../base/Singleton.h"
 
-std::function<void(const httplib::Request&, httplib::Response&)> HandlerFactory::createLoginHandler(LoginManager* loginManager) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        std::string result = loginManager->handle_login_request(req, res);
-        res.set_content(result, "text/plain");
-    };
-}
+std::function<void(const httplib::Request&, httplib::Response&)> HandlerFactory::createHandler(const Config::Handler& handler) {
+    if (handler.url == "/auth/login") {
+        return [](const httplib::Request& req, httplib::Response& res) {
+            Singleton<LoginManager>::getInstance().handle_login_request(req,res);
+            return ;
+        };
+    } else if (handler.url == "/auth/logout") {
+        return [](const httplib::Request& req, httplib::Response& res) {
+            Singleton<LoginManager>::getInstance().handle_logout_request(req,res);
+            return ;
+        };
+    } else if (handler.url == "/auth/verify_token") {
+        return [](const httplib::Request& req, httplib::Response& res) {
+            Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+        };
+    }
+    // } else if (handler.url == "/files/Dir") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/upload/FixedFile") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/upload/ChunkedFile") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/download/FixedFile") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/download/ChunkedFile") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/delete") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/create-directory") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/rename") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/move") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // } else if (handler.url == "/files/copy") {
+    //     return [](const httplib::Request& req, httplib::Response& res) {
+    //         Singleton<LoginManager>::getInstance().token_verification_middleware(req,res);
+    //         return true;  // or false, depending on the result
+    //     };
+    // }
 
-std::function<void(const httplib::Request&, httplib::Response&)> HandlerFactory::createLogoutHandler(LoginManager* loginManager) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        loginManager->handle_logout_request(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createTokenVerificationMiddleware(LoginManager* loginManager) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return loginManager->token_verification_middleware(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createSendFixedFileHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->SendFixedFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createSendChunkedFileHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->SendChunkedFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createSendcHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->Sendc(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createReceiveFixedFileHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->ReceiveFixedFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createReceiveChunkedFileHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->ReceiveChunkedFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createReceiveDirectoryHandler(FileTransmission* fileTransmission) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileTransmission->ReceiveDirectory(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createCreateDirectoryHandler(FileManageRequest* fileManageRequest) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileManageRequest->handleCreateDirectory(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createRenameFileHandler(FileManageRequest* fileManageRequest) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileManageRequest->handleRenameFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createMoveFileHandler(FileManageRequest* fileManageRequest) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileManageRequest->handleMoveFile(req, res);
-    };
-}
-
-std::function<bool(const httplib::Request&, httplib::Response&)> HandlerFactory::createCopyFileHandler(FileManageRequest* fileManageRequest) {
-    return [=](const httplib::Request& req, httplib::Response& res) {
-        return fileManageRequest->handleCopyFile(req, res);
+    // Default handler
+    return [](const httplib::Request& req, httplib::Response& res) {
+        res.status = 404;
     };
 }
--- src/Server/HandlerFactory.h
+++ src/Server/HandlerFactory.h
@@ -4,6 +4,8 @@
 #include <functional>
 #include <memory>
 #include "../include/httplib.h"
+#include "../base/Config.h"
+
 
 class LoginManager;
 class FileTransmission;
@@ -11,19 +13,7 @@ class FileManageRequest;
 
 class HandlerFactory {
 public:
-    static std::function<void(const httplib::Request&, httplib::Response&)> createLoginHandler(LoginManager* loginManager);
-    static std::function<void(const httplib::Request&, httplib::Response&)> createLogoutHandler(LoginManager* loginManager);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createTokenVerificationMiddleware(LoginManager* loginManager);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createSendFixedFileHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createSendChunkedFileHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createSendcHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createReceiveFixedFileHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createReceiveChunkedFileHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createReceiveDirectoryHandler(FileTransmission* fileTransmission);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createCreateDirectoryHandler(FileManageRequest* fileManageRequest);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createRenameFileHandler(FileManageRequest* fileManageRequest);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createMoveFileHandler(FileManageRequest* fileManageRequest);
-    static std::function<bool(const httplib::Request&, httplib::Response&)> createCopyFileHandler(FileManageRequest* fileManageRequest);
+    static std::function<void(const httplib::Request&, httplib::Response&)> createHandler(const Config::Handler& handler);
 };
 
 #endif  // HANDLER_FACTORY_H
