70cb2c43f1115b3b99131ba69677e66367b2fb1d
liangxinglong <liangxinglong@scutech.com>
2023-08-10 17:35:04 +0800

    拆分ftpserver文件

--- src/Server/FtpServer.h
+++ src/Server/FtpServer.h
@@ -13,72 +13,14 @@
 #include "../base/Singleton.h"
 class FtpServer {
 public:
-    FtpServer(std::string path):
-      server_(),
-      config_(path)
-    {
-    }
+    FtpServer(std::string path);
 
-    void start() {
-        // Create and configure the server
-        configureServer(server_);
-
-        // Start the server
-        server_.listen(config_.address(), 8080);
-    }
+    void start();
 
 private:
     Config config_;
     httplib::Server server_;
-    void configureServer(httplib::Server& server) {
-        // Set the server's run user
-        
-        uid_t currentUserId=getuid();
-
-        if (currentUserId == 0 && config_.run_user()!="") {
-                // Get the user ID for the specified user
-            struct passwd* pw = getpwnam(config_.run_user().c_str());
-            if (pw == nullptr) {
-                std::string errorMsg = "Error:User '"+config_.run_user()+"' not found.";
-                Singleton<Logger>::getInstance().Debug(errorMsg);
-                exit(1);
-            } else {
-                // Change the user ID of the process
-                if (setuid(pw->pw_uid) != 0) {
-                    std::string errorMsg="Error: Failed to set user ID for '"+config_.run_user() +"'.";
-                    Singleton<Logger>::getInstance().Debug(errorMsg);
-                    exit(1);
-                } 
-            }
-        } 
-
-        // Set the server's concurrency limit
-        if (config_.concurrency_limit() > 0) {
-            int concurrency_limit=config_.concurrency_limit();
-            server_.new_task_queue=[concurrency_limit ](){return new httplib::ThreadPool(concurrency_limit);};
-        }
-
-        // Set the server's keep-alive limit
-        if (config_.keepalive_limit() > 0) {
-            server.set_keep_alive_max_count(config_.keepalive_limit());
-        }
-
-        // Define your routes and handlers here
-        server.Get("/", [this](const httplib::Request& req, httplib::Response& res) {
-            // Example handler
-            res.set_content("Hello, World!", "text/plain");
-        });
-
-        // Create handlers using HandlerFactory
-        HandlerFactory handlerFactory;
-
-        // Get the handlers from the factory and register them with the server
-        const std::vector<Config::Handler>& Handlers=config_.handlers();
-        for (const auto& Handler :Handlers) {
-            auto handler=handlerFactory.createHandler(Handler);
-            server.Get(Handler.url, handler);
-        }
-    }
+    void configureServer(httplib::Server& server);
 };
 
 #endif
